3768f3e3cad9e6be185ed37034dfec78a0ce6d57
satyapr93 <satyapr93@gmail.com>
2016-08-12 14:21:24 +0530

    fixed bugs in basics.sh

--- scripts/basics.sh
+++ scripts/basics.sh
@@ -5,10 +5,10 @@
 
 # EXECUTION
 
-"printf "Setting system label and name...\n"
+#printf "Setting system label and name...\n"
 
-"sudo scutil --set ComputerName $SYSTEM_LABEL
-"sudo scutil --set HostName $SYSTEM_NAME
-"sudo scutil --set LocalHostName $SYSTEM_NAME
+#sudo scutil --set ComputerName $SYSTEM_LABEL
+#sudo scutil --set HostName $SYSTEM_NAME
+#sudo scutil --set LocalHostName $SYSTEM_NAME
 
-"sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $SYSTEM_NAME
+#sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $SYSTEM_NAME
