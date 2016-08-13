f6f45771857081220f4df541b09743af182c3cb1
satyapr93 <satyapr93@gmail.com>
2016-08-13 14:57:57 +0530

    docs for install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,18 @@
 # ./install.sh
 
 # EXECUTION
-./scripts/homebrew.sh
+
+printf "\n1> Basic settings...."
 ./scripts/basics.sh
+
+printf "\n2> Changing default setting preferences...."
 ./scripts/system_preferences.sh
+
+printf "\n3> Installing Hombrew and brew apps...."
+./scripts/homebrew.sh
+
+printf "\n4> Settings up shell, git and other dotfiles...."
 ./scripts/setup.sh
+
+printf "\n5> Installing macOS apps...."
 ./scripts/application.sh
